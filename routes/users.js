26ea2c4998f9a61535bb1c8d43ff1326b0f9d14a
natali-a-lvarez <03nataliaalvarez07@gmail.com>
2024-02-21 15:35:14 -0500

    creating server side validation for /users POST

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const userRouter = express.Router();
 const { User } = require("../models/index");
 
+const { check, validationResult } = require("express-validator");
+
 userRouter.use(express.json());
 userRouter.use(express.urlencoded({ extended: true }));
 
@@ -15,10 +17,24 @@ userRouter.get("/:id", async (req, res) => {
   res.json(user);
 });
 
-userRouter.post("/", async (req, res) => {
-  const newUser = await User.create(req.body);
-  res.json(newUser);
-});
+userRouter.post(
+  "/",
+  [check("name").not().isEmpty().trim(), check("age").not().isEmpty().trim()],
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.json({ error: errors.array() });
+      } else {
+        const newUser = await User.create(req.body);
+        const allUsers = await User.findAll();
+        res.json(allUsers);
+      }
+    } catch (e) {
+      next(e);
+    }
+  }
+);
 
 userRouter.put("/:id", async (req, res) => {
   const updatedUser = await User.update(req.body, {
